8081a020ab4d81812bb0354e1f702a8f977d19fd
Shashi Sharma <shashikant.sharma939@gmail.com>
2021-07-18 17:57:27 +0530

    Commit

--- src/test/java/com/sample/cucumber/utils/Utilities.java
+++ src/test/java/com/sample/cucumber/utils/Utilities.java
@@ -4,20 +4,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Utilities {
 
 	public static WebDriver driver;
 	public static Properties prop;
+	public static String testURL;
 
-	public void readPropertiesData() {
+	public Utilities() {
 		try {
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(
 					System.getProperty("user.dir") + "/src/test/resources/UAT/config.properties");
-			prop.load(ip);
+			Utilities.prop.load(ip);
+			testURL = prop.getProperty("testURL");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -25,6 +29,28 @@ public class Utilities {
 		}
 	}
 
-	public static String testURL = prop.getProperty("testURL");
+	public static void setUp() throws IOException {
+		// To Launch Chrome driver instance
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// Maximize Window
+		driver.manage().window().maximize();
+
+		// Default Page loading timeout
+		driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+
+		// Implicit Wait
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+		// Launch the Browser
+		driver.get(testURL);
+	}
+
+	public static void tearDown() {
+		// Close the browser sessions
+		driver.close();
+		driver.quit();
+	}
 
 }
--- src/test/resources/features/SampleTest.feature
+++ src/test/resources/features/SampleTest.feature
@@ -1,10 +1,10 @@
 Feature: Validate the Values and Total Balance
 
-Scenario: To verify the right count of values appear on the screen
-	Given User navigates to test Url
-	Then Ensure correct count of values appears on the screen
-	Then Check the values on the screen are greater than Zero
-	Then Verify Total balance is correct based on the values listed on the screen
-	|1000000.00|
-	Then Ensure values are formatted as currencies
-	Then Verify Total balance matches the sum of the values
+  Scenario: To verify the right count of values appear on the screen
+    Given User navigates to test Url
+    Then Ensure correct count of values appears on the screen
+    Then Check the values on the screen are greater than Zero
+    Then Verify Total balance is correct based on the values listed on the screen
+      | 1000000.00 |
+    Then Ensure values are formatted as currencies
+    Then Verify Total balance matches the sum of the values
